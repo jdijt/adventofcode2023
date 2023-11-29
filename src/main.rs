a9d4632629544256e3645be510c1b7fc293193a3
Jasper Dijt <jasper@jasperdijt.eu>
2023-11-29 12:21:51 +0100

    Add args parsing  + test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use std::io::{stdout, BufWriter};
-
 use ferris_says::say;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(last=true)]
+    assignment: String
+}
 
 fn main() {
+    let args = Args::parse();
     let stdout = stdout();
-    let message = String::from("Hello Jasper!");
+    let mut message = String::from("Hello ");
+    message.push_str(&args.assignment);
+
     let width = message.chars().count();
 
     let writer = BufWriter::new(stdout.lock());
