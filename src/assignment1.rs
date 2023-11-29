7220f1a0b0708894dd7440da6e510b897833312b
Jasper Dijt <jasper@jasperdijt.eu>
2023-11-29 20:23:45 +0100

    Basic structure

---
+++ src/assignment1.rs
@@ -0,0 +1,5 @@
+use std::{time::Duration, thread::sleep};
+
+pub fn run() {
+    sleep(Duration::from_secs(2))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-use std::io::{stdout, BufWriter};
-use ferris_says::say;
+use std::time::Instant;
 use clap::Parser;
+
+mod assignment1;
 
 #[derive(Parser)]
 struct Args {
-    #[arg(last=true)]
-    assignment: String
+    #[arg(last = true)]
+    assignment: String,
+}
+
+fn run_timed(func: fn()) {
+    println!("Starting\n");
+    let now= Instant::now();
+    func();
+    let elapsed = now.elapsed();
+    println!("\nDone!");
+    println!("Total time: {} micros", elapsed.as_micros())
 }
 
 fn main() {
     let args = Args::parse();
-    let stdout = stdout();
-    let mut message = String::from("Hello ");
-    message.push_str(&args.assignment);
 
-    let width = message.chars().count();
-
-    let writer = BufWriter::new(stdout.lock());
-    let _ = say(&message, width, writer);
+    match args.assignment.as_str() {
+        "1" => run_timed(assignment1::run),
+        o => println!("Unknown assignment {}", o),
+    }
 }
